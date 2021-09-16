6d3ddc3958f1546c479fb0ae9d3707fbdb10d91d
sina <sina8013@gmail.com>
2021-09-16 11:51:29 +0430

    just add image

--- lib/_home/homeMobile.dart
+++ lib/_home/homeMobile.dart
@@ -10,6 +10,37 @@ class HomeMobile extends StatefulWidget {
 class _HomeMobileState extends State<HomeMobile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+          width: _width,
+          height: _height,
+          color: Colors.amber,
+          child: Center(
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+              child: Center(
+                  child: FittedBox(
+                fit: BoxFit.cover,
+                clipBehavior: Clip.hardEdge,
+                child: Container(
+
+                    // color: Colors.red,
+                    child: Row(
+                  children: [
+                    Text(
+                      "datkjhjdhfjdhfjdhfa",
+                      style: TextStyle(fontSize: 300),
+                    ),
+                    Image(image: AssetImage('assets/img.jpg'))
+                  ],
+                )),
+              )),
+            ),
+          )),
+    );
   }
 }
